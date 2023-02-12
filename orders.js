ffaec7e19fa500eae2f7715e50c4975bd9224619
Parasaran <20euec103@skcet.ac.in>
2023-02-12 11:34:06 +0530

    completed

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName :'Foldable Mini Drone',
+        productNumber : '85631',
+        paymentStatus : 'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName :'LARVENDER KF102 Drone',
+        productNumber : '36574',
+        paymentStatus : 'Refunded',
+        shipping : 'Declined'
+    },
+    {
+        productName :'Ruko F11 Pro Drone',
+        productNumber : '45363',
+        paymentStatus : 'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName :'Agri Drone',
+        productNumber : '48631',
+        paymentStatus : 'Paid',
+        shipping : 'Delivered'
+    },
+    {
+        productName :'GPS 4k Drone',
+        productNumber : '18631',
+        paymentStatus : 'Paid',
+        shipping : 'Delivered'
+    },
+]
